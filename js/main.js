e99a69863953ffc41917ebd93e6e8ecd3cc6cfc0
yaominguo <missgmy@yahoo.com>
2017-05-06 02:06:32 +0800

    帮朋友做的一个小demo
    龍鳯祥的母亲节推广

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('body').css({width:$(window).width(),height:$(window).height()});
+});
+$('#musicbtn').click(function(){
+    if($(this).hasClass("play")){
+        $(this).css("animation-play-state","paused").removeClass("play");
+        $('.music')[0].pause();
+    }else{
+        $(this).css("animation-play-state","running").addClass("play");
+        $('.music')[0].play();
+    }
+});
+var mySwiper = new Swiper('.swiper-container', {
+    pagination : '.swiper-pagination',
+    onSlideChangeStart: function(swiper){
+        $('.swiper-slide').eq(swiper.activeIndex).addClass("animate").siblings().removeClass("animate");
+        if(swiper.activeIndex==7){
+            $('#button').hide();
+        }else{
+            $('#button').show();
+        }
+    }
+});
