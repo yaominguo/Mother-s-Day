016580e3297dab43432659253933775016f12fd9
yaominguo <missgmy@yahoo.com>
2017-05-06 13:21:30 +0800

    更改了下ios微信音频无法自动播放的问题

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
+
 $(document).ready(function(){
     $('body').css({width:$(window).width(),height:$(window).height()});
-    $('.music')[0].play();
 });
+window.onload=function(){
+    document.getElementById('music').play(); 
+    document.addEventListener("WeixinJSBridgeReady", function () { 
+        document.getElementById('music').play(); 
+    }, false);
+}
 $('#musicbtn').click(function(){
     if($(this).hasClass("play")){
         $(this).css("animation-play-state","paused").removeClass("play");
-        $('.music')[0].pause();
+        $('#music')[0].pause();
     }else{
         $(this).css("animation-play-state","running").addClass("play");
-        $('.music')[0].play();
+        $('#music')[0].play();
     }
 });
 var mySwiper = new Swiper('.swiper-container', {
